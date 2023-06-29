b99b9b163b8531f7f5d28a2b07ea6d335ff8fd2a
sabbir-dcy <sabbir.d3v@gmail.com>
2023-06-30 05:52:32 +0600

    heck

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 require("dotenv").config();
 const port = process.env.PORT || 3000;
 app.use(cors());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 app.get("/home", (req, res) => {
   res.json({ success: true });
