1a08e187a183ee01d0ba792d7496c0bec65b6ccc
sabbir-dcy <sabbir.d3v@gmail.com>
2023-06-30 06:21:50 +0600

    another

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
-import express from "express";
-
+const express = require("express");
 const app = express();
+const path = require("path");
 const port = 5000;
+
+// app.use(express.static(path.join(__dirname + '/public')));
 
 app.get("/", (req, res) => {
   res.json({ success: false });
 });
 
 app.get("/home", (req, res) => {
-  res.json({ home: true });
+  res.json({ home: false });
 });
 
 app.listen(port, () => {
