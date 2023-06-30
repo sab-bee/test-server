1e136cda780c1cc56ac002d898278e2140de3abb
sabbir-dcy <sabbir.d3v@gmail.com>
2023-06-30 06:23:24 +0600

    another

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const port = 5000;
 
-// app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/public')));
 
 app.get("/", (req, res) => {
   res.json({ success: false });
