47568f270665aa9483136e46f015e0cabe423c57
sabbir-dcy <sabbir.d3v@gmail.com>
2023-06-30 06:11:45 +0600

    another

--- index.js
+++ index.js
@@ -1,15 +1,12 @@
-const express = require("express");
+import express from "express";
+
 const app = express();
-const path = require("path");
-require("dotenv").config();
-const port = process.env.PORT || 3000;
+const port = 5000;
 
-app.use(express.static(path.join(__dirname, "/public")));
-
-app.get("/home", (req, res) => {
-  res.send({ data: "hey" });
+app.get("/", (req, res) => {
+  res.json({ success: true });
 });
 
 app.listen(port, () => {
-  console.log("listening to port", port);
+  console.log("listening", port);
 });
--- public/app.js
+++
@@ -1,3 +0,0 @@
-fetch('/home')
-  .then((res) => res.json())
-  .then((data) => console.log(data));
