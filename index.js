8890376465161d3835e6082b03b2453c61f3430e
sabbir-dcy <sabbir.d3v@gmail.com>
2023-06-30 06:55:53 +0600

    e

--- index.js
+++ index.js
@@ -8,30 +8,42 @@ app.use(express.static(path.join(__dirname + "/public")));
 
 let players = [];
 
-const uri = "https://prosettings.net/lists/valorant/";
-tabletojson.convertUrl(uri, (tableData) => {
-  players = tableData[0];
-
+async function server() {
   app.get("/", (req, res) => {
-    res.json(players);
+    res.json({ success: false });
   });
 
-  app.get("/player", (req, res) => {
-    const query = req.query;
-    const data = players.find((p) => str(p.Player) === query.name);
-    data ? res.json(data) : res.json({ failed: true });
+  app.get("/home", (req, res) => {
+    res.json({ home: false });
   });
 
-  app.get("/team", (req, res) => {
-    const query = req.query;
-    const data = players.filter((p) => str(p.Team) === query.name);
-    checked(data) ? res.json(data) : res.json({ failed: true });
-  });
+  const uri = "https://prosettings.net/lists/valorant/";
+  tabletojson.convertUrl(uri, (tableData) => {
+    players = tableData[0];
 
-  app.listen(port, () => {
-    console.log(`Listening on port ${port}`);
+    app.get("/", (req, res) => {
+      res.json(players);
+    });
+
+    app.get("/player", (req, res) => {
+      const query = req.query;
+      const data = players.find((p) => str(p.Player) === query.name);
+      data ? res.json(data) : res.json({ failed: true });
+    });
+
+    app.get("/team", (req, res) => {
+      const query = req.query;
+      const data = players.filter((p) => str(p.Team) === query.name);
+      checked(data) ? res.json(data) : res.json({ failed: true });
+    });
+
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`);
+    });
   });
-});
+}
+
+server();
 
 function str(data) {
   return data.toLowerCase();
@@ -40,3 +52,4 @@ function str(data) {
 function checked(data) {
   return data.length;
 }
+
